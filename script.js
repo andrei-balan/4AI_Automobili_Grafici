7c7c89baf08842ce3e5bd62a861912a5fee33103
andre <78569120+andrei-balan@users.noreply.github.com>
2023-04-26 09:23:09 +0200

    aggiunta query lato php

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var _btnCarica = null;
-var _inputFile = null;
+var _inputFile = null, _main = null;
+var urlBase = window.location.href;
 
 window.onload = function(){
     _btnCarica = document.getElementsByTagName("button")[0];
@@ -7,8 +8,7 @@ window.onload = function(){
 
     //input[type=file] -> prelevo il primo input di tipo file
     _inputFile = document.querySelector("input[type=file]");
-
-
+    _main = document.querySelector("main");    
 };
 
 function onbtnCarica(){
@@ -19,66 +19,71 @@ function onbtnCarica(){
 
     let reader = new FileReader();
     //Indico alla libreria chi contattare terminata la lettura
-    reader.onload =  function(datiletti){
-        //console.log(datiletti);
-        //console.log(datiletti.currentTarget.result);
+    reader.onload = async function(datiletti){
+        //console.log(datiletti);// Oggetto FileReader
+        console.log(datiletti.currentTarget.result); //risultati codificati
+        let dati = datiletti.currentTarget.result.split("/");
 
-        let dati = datiletti.currentTarget.result.split(",");
-        console.log(atob(dati[1]));
+        //Trasformato da base64 utf8
+        let datiDecodificati = atob(dati[2]);
 
-        let datiDecodificati = atob(dati[1]);
-
-        console.log(datiDecodificati.split("\n"));
-
+        //Divido le righe del file in array
         let righe = datiDecodificati.split("\r\n");
-        let record = [];
-        let colonne = [];
 
+        //Divido le colonne di ciascuna riga
+        /*
+            array di array
+            array esterno: righe del file
+            array interno: colonne di ciascuna riga
+        */
 
-        
-
-        for(let i = 0; i < righe.length; i++){
-            righe[i] = righe[i].replaceAll("\"","");
-            colonne = righe[i].split(",");
+        let record = [], colonne = [];
+        for(let riga of righe){
+            riga = riga.replaceAll("\"", "");
+            colonne = riga.split(",");
             record.push(colonne);
         }
+        console.log(record);
 
+        //Contatto il server
+        //Proviamo a connetterci al server
+        let busta = await fetch(urlBase + "server/inserisciMezzo.php", {
+                method:"post",
+                body:JSON.stringify(record[2])
+            }
+        );
 
-        console.log(record)
-        let main = document.getElementById("main");
-               //Creazione dinamica della tabella: 1,3,5,7,8
-               let tabella = document.createElement("table");
-               let thead = document.createElement("thead");
-               let tbody = document.createElement("tbody");
-               tabella.appendChild(thead);
-               tabella.appendChild(tbody);
-                main.appendChild(tabella);
-       
-               //Intestazione
-               let intestazione = "<tr>";
-               for(let idColonna in record[0]){
-                   if([1,3,5,7,8].includes(parseInt(idColonna)))
-                       intestazione += `<th>${record[0][idColonna]}</th>`;
-               }
-               intestazione += "</tr>";
-               thead.innerHTML = intestazione;
-       
-               //Dati
-               dati = "";
-               for(let i=1; i< record.length; i++){
-                   dati += "<tr>";
-                   for(let idcolonna in record[i]){
-                       if([1,3,5,7,8].includes(parseInt(idcolonna)))
-                           dati += `<td>${record[i][idcolonna]}</td>`;
-                   }
-                   dati += "</tr>";
-               }
-               tbody.innerHTML = dati;
-        
+        //Leggo il contenuto della busta
+        console.log(await busta.json());
 
+        //Creazione dinamica della tabella: 1,3,5,7,8
+        let tabella = document.createElement("table");
+        let thead = document.createElement("thead");
+        let tbody = document.createElement("tbody");
+        tabella.appendChild(thead);
+        tabella.appendChild(tbody);
+        _main.appendChild(tabella);
 
-        
+        //Intestazione
+        let intestazione = "<tr>";
+        for(let idColonna in record[0]){
+            if([1,3,5,7,8].includes(parseInt(idColonna)))
+                intestazione += `<th>${record[0][idColonna]}</th>`;
+        }
+        intestazione += "</tr>";
+        thead.innerHTML = intestazione;
 
+        //Dati
+        dati = "";
+        for(let i=1; i< record.length; i++){
+            dati += "<tr>";
+            for(let idcolonna in record[i]){
+                if([1,3,5,7,8].includes(parseInt(idcolonna)))
+                    dati += `<td>${record[i][idcolonna]}</td>`;
+            }
+            dati += "</tr>";
+        }
+        tbody.innerHTML = dati;
     };
 
     //Passo il file e avvio la lettura
